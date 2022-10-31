6d0730559a9e1d194ad85c134f594c7d26e79b92
tanyaponkomkham <65309010032@tatc.ac.th>
2022-10-31 10:47:21 +0700

    first

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import generatedRoutes from 'virtual:generated-pages'
+import { setupLayouts } from 'virtual:generated-layouts'
+
+import 'windi.css'
+
+import App from './app.vue'
+
+const routes = setupLayouts(generatedRoutes)
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+  .use(router)
+
+Object.values(import.meta.globEager('./modules/*.js')).map(i => i.install?.({ app }))
+
+app.mount('#app')
+
+
+
